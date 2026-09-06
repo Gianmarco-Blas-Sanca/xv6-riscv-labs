91de1f6d2cfa5886aacf4dd900eedcef4d2cf4b1
Gian <gianmarcoblassanca@gmail.com>
2026-09-06 00:07:43 -0500

    Lab 2 Ejercicio 1

--- kernel/defs.h
+++ kernel/defs.h
@@ -60,7 +60,7 @@ void            ireclaim(int);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
-
+uint64          count_free_bytes(void);
 // log.c
 void            initlog(int, struct superblock*);
 void            log_write(struct buf*);
